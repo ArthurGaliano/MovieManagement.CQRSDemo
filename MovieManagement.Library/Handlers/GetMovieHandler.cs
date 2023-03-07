b0e142fa880120f7773d9500df0487096ac686b7
Arthur Galiano <arthurgali99@gmail.com>
2023-03-07 18:26:48 -0500

    Add project files.

---
+++ MovieManagement.Library/Handlers/GetMovieHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using MovieManagement.Library.Data;
+using MovieManagement.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieManagement.Library.Queries
+{
+    public class GetMovieHandler : IRequestHandler<GetMovieListQuery, List<MovieModel>>
+    {
+        private readonly IDataRepository _dataRepository;
+        public GetMovieHandler(IDataRepository dataRepository) 
+        {
+            _dataRepository = dataRepository;
+        }
+        public Task<List<MovieModel>> Handle(GetMovieListQuery request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(_dataRepository.GetMovies());
+        }
+    }
+}
